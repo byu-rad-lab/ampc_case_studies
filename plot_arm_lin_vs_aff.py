3cb82dcd08a953b572176f016bc4c21c932c4ec3
Mat Haskell <mhaskell9@gmail.com>
2024-10-09 11:40:10 -0600

    Updated to remove

--- plot_arm_lin_vs_aff.py
+++ plot_arm_lin_vs_aff.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from examples.single_link_arm.plotter import Plotter
+from examples.single_link_arm.plotter import Plotter, PlotWindow
 from parsing import getParsedArgs_plot
 
 
@@ -16,55 +16,59 @@ def plot(load_dir: str):
     time = np.loadtxt(load_dir + 'time.txt')
     costs = np.loadtxt(load_dir + 'costs.txt')
     aff_states = np.loadtxt(load_dir + 'aff_states.txt')
+    aff_inputs = np.loadtxt(load_dir + 'aff_inputs.txt')
+    lin_inputs = np.loadtxt(load_dir + 'lin_inputs.txt')
     lin_states = np.loadtxt(load_dir + 'lin_states.txt')
     xr_hist = np.loadtxt(load_dir + 'ref_states.txt')
 
-    plotter = Plotter(time, legend, deg, fontsize, figsize)
+    app = Plotter(fontsize)
+    pw = PlotWindow(time, legend, deg, fontsize, figsize)
+    pw2 = PlotWindow(time, legend, deg, fontsize, figsize)
 
     c_list = np.linspace(0, 1, len(costs[0]))
-    plotter.plotCosts(c_list, costs[0], label='affinized')
-    plotter.plotCosts(c_list, costs[1], '-.', label='linearized')
+    pw.plotCosts(c_list, costs[0], label='affinized')
+    pw.plotCosts(c_list, costs[1], '-.', label='linearized')
 
     idx = np.argmin(costs, axis=1)
     aff_mc = costs[0,idx[0]]
     lin_mc = costs[1,idx[1]]
-    print(f'min aff cost = {aff_mc} at c = {c_list[idx[0]]:.1f}')
-    print(f'min lin cost = {lin_mc} at c = {c_list[idx[1]]:.1f}')
 
-    if idx[0] == 0:
-        aff_improve = 0.0
-    else:
-        aff_improve = 1 - aff_mc/costs[0,0]
-    if idx[1] == 0:
-        lin_improve = 0.0
-    else:
-        lin_improve = 1 - lin_mc/costs[1,0]
-    print(f'aff improvement: {aff_improve*100:.2f}%')
-    print(f'lin improvement: {lin_improve*100:.2f}%')
+    aff_improve = (1 - aff_mc/costs[0,0]) * 100
+    lin_improve = (1 - lin_mc/costs[1,0]) * 100
 
-    diff = aff_mc - lin_mc
-    if np.abs(diff) < 1e-6:
-        better = 'aff/lin'
-        imp = 0.0
-    elif diff <= 0:
-        better = 'aff'
-        imp = 1 - aff_mc / lin_mc
-    else:
-        better = 'lin'
-        imp = 1 - lin_mc / aff_mc
-    print(f'best_method: {better} by {imp*100:.2f}%')
+    print(f'min aff cost = {aff_mc} @ c = {c_list[idx[0]]:.1f} ({aff_improve:.2f}% better than c=0)')
+    print(f'min lin cost = {lin_mc} at c = {c_list[idx[1]]:.1f} ({lin_improve:.2f}% better than c=0)')
+
+    aff_improve = 1 - aff_mc / lin_mc
+    print(f'aff improvement: {aff_improve:.2f}%')
 
     aff_states = aff_states.reshape(len(c_list),-1,2)
+    aff_inputs = aff_inputs.reshape(len(c_list),-1,1)
     lin_states = lin_states.reshape(len(c_list),-1,2)
+    lin_inputs = lin_inputs.reshape(len(c_list),-1,1)
     for i,c in enumerate(c_list):
-        plotter.plotStateLine(aff_states[i], label=f'aff: {c = :.1f}')
-        plotter.plotStateLine(lin_states[i], '-.', label=f'lin: {c = :.1f}')
-    plotter.plotStateLine(xr_hist, 'r--', label='ref')
+        pw.plotStateLine(aff_states[i], label=f'aff: {c = :.1f}')
+        pw.plotStateLine(lin_states[i], '-.', label=f'lin: {c = :.1f}')
+    pw.plotStateLine(xr_hist, 'r--', label='ref')
 
-    plotter.show()
+    app.addWindow(pw)
 
+    pw2.plotStateLine(time, aff_states[idx[0]], label=f'aff (uk): c = {c_list[idx[0]]:.1f}')
+    pw2.plotStateLine(time, lin_states[idx[1]], '-.', label=f'lin: c = {c_list[idx[1]]:.1f}')
+
+    pw2.plotInputLine(time, aff_inputs[aff_uk_idx], label=f'aff (uk): c = {c_list[aff_uk_idx]:.1f}')
+    pw2.plotInputLine(time, lin_inputs[lin_idx], '-.', label=f'lin: c = {c_list[lin_idx]:.1f}')
+
+    pw2.plotStateLine(time, xr_hist, 'r--', label='ref')
+
+    app.addWindow(pw2)
+    app.show()
+
+    print('Saving plots...')
     for ext in img_types:
-        plotter.savePlots(load_dir, image_type=ext)
+        pw.savePlots(load_dir, image_type=ext)
+    print('Plots saved')
+
     return
 
 
--- plot_multirotor_lin_vs_aff.py
+++ plot_multirotor_lin_vs_aff.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from examples.multirotor.plotter import Plotter
+from examples.multirotor.plotter import Plotter, PlotWindow
 from parsing import getParsedArgs_plot
 
 
@@ -36,14 +36,16 @@ def plot(load_dir: str):
     lin_inputs = np.loadtxt(load_dir + 'lin_inputs.txt')
     lin_solve_times = np.loadtxt(load_dir + 'lin_solve_times.txt')
 
-    plotter = Plotter(legend, deg, fontsize, figsize)
+    app = Plotter(fontsize)
+    pw = PlotWindow(legend, deg, fontsize, figsize)
+    pw2 = PlotWindow(legend, deg, fontsize, figsize)
 
     num_c = len(aff_uk_costs)
     c_list = np.linspace(0, 1, num_c)
-    plotter.plotCosts(c_list, aff_uk_costs, label='aff (uk)')
-    plotter.plotCosts(c_list, aff_ueq_costs, label='aff (ueq)')
-    plotter.plotCosts(c_list, aff_xeq_costs, label='aff (xeq)')
-    plotter.plotCosts(c_list, lin_costs, '-.', label='lin')
+    pw.plotCosts(c_list, aff_uk_costs, label='aff (uk)')
+    pw.plotCosts(c_list, aff_ueq_costs, label='aff (ueq)')
+    pw.plotCosts(c_list, aff_xeq_costs, label='aff (xeq)')
+    pw.plotCosts(c_list, lin_costs, '-.', label='lin')
 
     aff_uk_idx = np.argmin(aff_uk_costs)
     aff_ueq_idx = np.argmin(aff_ueq_costs)
@@ -87,24 +89,43 @@ def plot(load_dir: str):
     aff_xeq_inputs = aff_xeq_inputs.reshape(num_c,-1,4)
     lin_inputs = lin_inputs.reshape(num_c,-1,4)
     for i,c in enumerate(c_list):
-        plotter.plotStateLine(time, aff_uk_states[i], label=f'aff (uk): {c = :.1f}')
-        plotter.plotStateLine(time, aff_ueq_states[i], label=f'aff (ueq): {c = :.1f}')
-        plotter.plotStateLine(time, aff_xeq_states[i], label=f'aff (xeq): {c = :.1f}')
-        plotter.plotStateLine(time, lin_states[i], '-.', label=f'lin: {c = :.1f}')
-        plotter.plotInputLine(time, aff_uk_inputs[i], label=f'aff (uk): {c = :.1f}')
-        plotter.plotInputLine(time, aff_ueq_inputs[i], label=f'aff (ueq): {c = :.1f}')
-        plotter.plotInputLine(time, aff_xeq_inputs[i], label=f'aff (xeq): {c = :.1f}')
-        plotter.plotInputLine(time, lin_inputs[i], '-.', label=f'lin: {c = :.1f}')
-        plotter.plotSolveTimes(time, aff_uk_solve_times[i], label=f'aff (uk): {c = :.1f}')
-        plotter.plotSolveTimes(time, aff_ueq_solve_times[i], label=f'aff (ueq): {c = :.1f}')
-        plotter.plotSolveTimes(time, aff_xeq_solve_times[i], label=f'aff (xeq): {c = :.1f}')
-        plotter.plotSolveTimes(time, lin_solve_times[i], '-.', label=f'lin: {c = :.1f}')
-    plotter.plotStateLine(time, xr_hist, 'r--', label='ref')
+        pw.plotStateLine(time, aff_uk_states[i], label=f'aff (uk): {c = :.1f}')
+        pw.plotStateLine(time, aff_ueq_states[i], label=f'aff (ueq): {c = :.1f}')
+        pw.plotStateLine(time, aff_xeq_states[i], label=f'aff (xeq): {c = :.1f}')
+        pw.plotStateLine(time, lin_states[i], '-.', label=f'lin: {c = :.1f}')
+        pw.plotInputLine(time, aff_uk_inputs[i], label=f'aff (uk): {c = :.1f}')
+        pw.plotInputLine(time, aff_ueq_inputs[i], label=f'aff (ueq): {c = :.1f}')
+        pw.plotInputLine(time, aff_xeq_inputs[i], label=f'aff (xeq): {c = :.1f}')
+        pw.plotInputLine(time, lin_inputs[i], '-.', label=f'lin: {c = :.1f}')
+        pw.plotSolveTimes(time, aff_uk_solve_times[i], label=f'aff (uk): {c = :.1f}')
+        pw.plotSolveTimes(time, aff_ueq_solve_times[i], label=f'aff (ueq): {c = :.1f}')
+        pw.plotSolveTimes(time, aff_xeq_solve_times[i], label=f'aff (xeq): {c = :.1f}')
+        pw.plotSolveTimes(time, lin_solve_times[i], '-.', label=f'lin: {c = :.1f}')
+    pw.plotStateLine(time, xr_hist, 'r--', label='ref')
 
-    plotter.show()
+    app.addWindow(pw)
 
+    pw2.plotStateLine(time, aff_uk_states[aff_uk_idx], label=f'aff (uk): c = {c_list[aff_uk_idx]:.1f}')
+    pw2.plotStateLine(time, aff_ueq_states[aff_ueq_idx], label=f'aff (ueq): c = {c_list[aff_ueq_idx]:.1f}')
+    pw2.plotStateLine(time, aff_xeq_states[aff_xeq_idx], label=f'aff (xeq): c = {c_list[aff_xeq_idx]:.1f}')
+    pw2.plotStateLine(time, lin_states[lin_idx], '-.', label=f'lin: c = {c_list[lin_idx]:.1f}')
+
+    pw2.plotInputLine(time, aff_uk_inputs[aff_uk_idx], label=f'aff (uk): c = {c_list[aff_uk_idx]:.1f}')
+    pw2.plotInputLine(time, aff_ueq_inputs[aff_ueq_idx], label=f'aff (ueq): c = {c_list[aff_ueq_idx]:.1f}')
+    pw2.plotInputLine(time, aff_xeq_inputs[aff_xeq_idx], label=f'aff (xeq): c = {c_list[aff_xeq_idx]:.1f}')
+    pw2.plotInputLine(time, lin_inputs[lin_idx], '-.', label=f'lin: c = {c_list[lin_idx]:.1f}')
+
+    pw2.plotStateLine(time, xr_hist, 'r--', label='ref')
+
+    app.addWindow(pw2)
+    app.show()
+
+    print('Saving plots...')
     for ext in img_types:
-        plotter.savePlots(load_dir, image_type=ext)
+        pw.savePlots(load_dir, end='_all', image_type=ext)
+        pw2.savePlots(load_dir, end='_best', image_type=ext)
+    print('Plots saved')
+
     return
 
 
