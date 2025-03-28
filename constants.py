217d37b286095316f743d65d30e603f3160aeebe
Mat Haskell <mhaskell9@gmail.com>
2025-03-28 13:23:39 -0600

    Moving constants to own file

---
+++ constants.py
@@ -0,0 +1,18 @@
+METHODS = ['best_aff', 'nom_aff', 'best_lin', 'nom_lin']
+ANCHOR_POINTS = ['(xt,ut)', '(xt,ue)', '(xe,ut)', '(xe,ue)']
+# COLORS = ['tab:pink', 'tab:blue', 'tab:orange', 'tab:green']
+# REF_COLOR = 'tab:red'
+
+COLORS = {
+    'best_aff': 'tab:pink',
+    'nom_aff': 'tab:blue',
+    'best_lin': 'tab:orange',
+    'nom_lin': 'tab:green',
+    'ref': 'tab:red',
+
+    0: 'tab:pink',
+    1: 'tab:blue',
+    2: 'tab:orange',
+    3: 'tab:green',
+    4: 'tab:red',
+}
