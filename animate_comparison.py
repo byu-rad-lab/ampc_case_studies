9d1c755435dafe4f2dd5f6244f1a344cbcea15be
Mat Haskell <mhaskell9@gmail.com>
2025-03-28 13:41:20 -0600

    Add single file to animate any system

---
+++ animate_comparison.py
@@ -0,0 +1,220 @@
+import numpy as np
+# from tqdm import tqdm
+import os
+import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+
+from case_studies.animation import StackedAnimation
+from case_studies.single_link_arm.animation import ArmAnimator
+from case_studies.block_beam.animation import BlockBeamAnimator
+from parsing import getParsedArgs_animate
+import constants as C
+
+
+def animate(load_dir: str, save_path: str, method:str|None, anchor_point: str,
+            c_idx: int, k_idx: int):
+    figsize = 800,800
+
+    ## Load Data
+    load_dir = os.path.expanduser(load_dir)
+    with open(os.path.join(load_dir, 'system.txt'), 'r') as f:
+        system = f.read()
+
+    time = np.load(os.path.join(load_dir, 'time.npy'))
+    xr_hist = np.load(os.path.join(load_dir, 'ref_states.npy'))
+
+    aff_uk_costs = np.load(os.path.join(load_dir, 'aff_uk_costs.npy'))
+    aff_ueq_costs = np.load(os.path.join(load_dir, 'aff_ueq_costs.npy'))
+    aff_xeq_costs = np.load(os.path.join(load_dir, 'aff_xeq_costs.npy'))
+    lin_costs = np.load(os.path.join(load_dir, 'lin_costs.npy'))
+
+    aff_uk_min_idx = np.unravel_index(np.argmin(aff_uk_costs), aff_uk_costs.shape)
+    aff_ueq_min_idx = np.unravel_index(np.argmin(aff_ueq_costs), aff_ueq_costs.shape)
+    aff_xeq_min_idx = np.unravel_index(np.argmin(aff_xeq_costs), aff_xeq_costs.shape)
+    lin_min_idx = np.unravel_index(np.argmin(lin_costs), lin_costs.shape)
+
+    min_costs = [aff_uk_costs[aff_uk_min_idx], aff_ueq_costs[aff_ueq_min_idx],
+                 aff_xeq_costs[aff_xeq_min_idx], lin_costs[lin_min_idx]]
+    idx = np.argmin(min_costs)
+    if idx == 0:
+        anchor_pt = '(xt,ut)'
+        states = np.load(os.path.join(load_dir, 'aff_uk_states.npy'))
+        states_top = states[aff_uk_min_idx]
+        best_idx = aff_uk_min_idx
+    elif idx == 1:
+        anchor_pt = '(xt,ue)'
+        states = np.load(os.path.join(load_dir, 'aff_ueq_states.npy'))
+        states_top = states[aff_ueq_min_idx]
+        best_idx = aff_ueq_min_idx
+    elif idx == 2:
+        anchor_pt = '(xe,ut)'
+        states = np.load(os.path.join(load_dir, 'aff_xeq_states.npy'))
+        states_top = states[aff_xeq_min_idx]
+        best_idx = aff_xeq_min_idx
+    else:
+        anchor_pt = '(xe,ue)'
+        states = np.load(os.path.join(load_dir, 'lin_states.npy'))
+        states_top = states[lin_min_idx]
+        best_idx = lin_min_idx
+
+    print(f'Top: {anchor_pt}, c={best_idx[1]/10:.1f}, k={best_idx[0]+1} \t Best Affinization')
+
+    if method in C.METHODS:
+        match method:
+            case 'best_aff':
+                anchor_point = anchor_pt
+                k_idx, c_idx = best_idx
+                label_bot = "Best Affinization"
+            case 'best_lin':
+                states = np.load(os.path.join(load_dir, 'lin_states.npy'))
+                k_idx, c_idx = best_idx
+                label_bot = "Best Linearization"
+            case 'nom_aff':
+                states = np.load(os.path.join(load_dir, 'aff_uk_states.npy'))
+                anchor_point = C.ANCHOR_POINTS[0]
+                k_idx = 0
+                c_idx = 0
+                label_bot = "Nominal Affinization"
+            case 'nom_lin':
+                states = np.load(os.path.join(load_dir, 'lin_states.npy'))
+                k_idx = 0
+                c_idx = 0
+                label_bot = "Nominal Linearization"
+            case _:
+                raise ValueError(f'Invalid method: {method}')
+        color_bot = C.COLORS[method]
+    else:
+        match anchor_point:
+            case '(xt,ut)':
+                states = np.load(os.path.join(load_dir, 'aff_uk_states.npy'))
+                costs = np.load(os.path.join(load_dir, 'aff_uk_costs.npy'))
+            case '(xt,ue)':
+                states = np.load(load_dir + 'aff_ueq_states.npy')
+                costs = np.load(load_dir + 'aff_ueq_costs.npy')
+            case '(xe,ut)':
+                states = np.load(load_dir + 'aff_xeq_states.npy')
+                costs = np.load(load_dir + 'aff_xeq_costs.npy')
+            case '(xe,ue)':
+                states = np.load(load_dir + 'lin_states.npy')
+                costs = np.load(load_dir + 'lin_costs.npy')
+            case _:
+                raise ValueError(f'Invalid approx_pt: {anchor_point}')
+        if c_idx == -1 and k_idx == -1:
+            k_idx,c_idx = np.unravel_index(np.argmin(costs), costs.shape)
+        elif c_idx == -1:
+            c_idx = np.argmin(costs[k_idx])
+        elif k_idx == -1:
+            k_idx = np.argmin(costs[:,c_idx])
+        color_bot = 'tab:gray'
+        label_bot = 'Manual Selection'
+
+    states_bot = states[k_idx, c_idx]
+    bot_info = f'{anchor_point}, c={c_idx/10:.1f}, k={k_idx+1} \t {label_bot}'
+    print(f'Bot: {bot_info}')
+    if label_bot == '':
+        label_bot = bot_info
+
+    states_top = states_top.T
+    states_bot = states_bot.T
+    xr_hist = xr_hist.T
+
+    # fig = plt.figure(figsize=(16,9), layout='tight')
+    fig = plt.figure(figsize=(19.2,10.8), layout='tight')
+
+    match system:
+        case 'arm':
+            Animator = ArmAnimator
+            data_idx = [0, 1]
+            x_labels = [r'$\theta$ (deg)', r'$\dot{\theta}$ (rad/s)']
+            wr = np.array([1, 1.5, 0.125])
+            hr = np.array([1, 1, 0.05, 1, 1])
+            ani_col = 0
+            data_col = 1
+            # gs = fig.add_gridspec(5, 3, width_ratios=[1, 1.5, 0.125],
+            #                       height_ratios=[1, 1, 0.05, 1, 1])
+            # ani_axes = [fig.add_subplot(gs[:2,0]), fig.add_subplot(gs[-2:,0])]
+            # data_axes = [[fig.add_subplot(gs[i,1]) for i in range(2)],
+            #              [fig.add_subplot(gs[-i,1]) for i in range(2,0,-1)]]
+        case 'blockbeam':
+            Animator = BlockBeamAnimator
+            data_idx = [0, 1]
+            x_labels = ['z (m)', r'$\theta$ (deg)']
+            wr = np.array([0.125, 1, 0.1, 1.5, 0.1])
+            hr = np.array([1, 1, 0.05, 1, 1])
+            ani_col = 1
+            data_col = 3
+            # gs = fig.add_gridspec(5, 5, width_ratios=wr, height_ratios=hr)
+            # ani_axes = [fig.add_subplot(gs[:2,1]), fig.add_subplot(gs[-2:,1])]
+            # data_axes = [[fig.add_subplot(gs[i,3]) for i in range(2)],
+            #              [fig.add_subplot(gs[-i,3]) for i in range(2,0,-1)]]
+        case 'cart':
+            # Animator = CartAnimator
+            data_idx = [0, 1]
+            x_labels = ['z (m)', r'$\theta$ (deg)']
+        case 'pendulum':
+            # Animator = PendulumAnimator
+            Q = np.load(os.path.join(load_dir, 'Q.npy'))
+            if Q[0] == 0.0: # controlling cart
+                data_idx = [0, 1]
+                x_labels = ['z (m)', r'$\theta$ (deg)']
+            else: # controlling pendulum
+                data_idx = [1, 3]
+                x_labels = [r'$\theta$ (deg)', r'$\dot{\theta}$ (rad/s)']
+            wr = np.array([0.125, 1, 0.1, 1.5, 0.1])
+            hr = np.array([1, 1, 0.05, 1, 1])
+            ani_col = 1
+            data_col = 3
+        case 'multirotor':
+            # Animator = MultirotorAnimator
+            data_idx = [1, 2, 3]
+            x_labels = ['p_y (m)', 'p_y (m)', r'$\phi$ (deg)']
+            wr = np.array([0.125, 1, 0.1, 1.5, 0.1])
+            hr = np.array([1, 1, 1, 0.05, 1, 1, 1])
+            ani_col = 1
+            data_col = 3
+        case _:
+            raise ValueError(f"System {system} not recognized.")
+
+    n = len(data_idx)
+    gs = fig.add_gridspec(len(hr), len(wr), width_ratios=wr, height_ratios=hr)
+    ani_axes = [fig.add_subplot(gs[:n,ani_col]), fig.add_subplot(gs[-n:,ani_col])]
+    data_axes = [[fig.add_subplot(gs[i,data_col]) for i in range(n)],
+                    [fig.add_subplot(gs[-i,data_col]) for i in range(n,0,-1)]]
+
+    x_hists = [states_top[data_idx], states_bot[data_idx]]
+    xr_hist = xr_hist[data_idx]
+    colors = [C.COLORS[0], color_bot]
+    labels = ['Best Affinization', label_bot]
+
+    ani = StackedAnimation(Animator, fig, ani_axes, data_axes, time, x_hists, x_labels, xr_hist, colors, labels)
+    if save_path is not None:
+        save_path = os.path.expanduser(save_path)
+        ani.save(save_path)
+        # print('',flush=True, end='')
+        print(f'Saved animation to {save_path}')
+    else:
+        ani.animate()
+
+def main():
+    load_dir = '~/data/ampc24/analysis/arm/step_60'
+    load_dir = '~/data/ampc24/analysis/arm/cos_90_3'
+    # load_dir = '~/data/ampc24/analysis/beam/step_0.4'
+    default_dir = '/tmp/ampc24/analysis/'
+    desc = '''
+        AniMate stacked AMPC simulations. The top animation is the best affinization and the bottom defaults to be nominal linearization, but a different modeling method can be manually selected.
+        '''
+    args = getParsedArgs_animate(load_dir, desc)
+    print(args)
+    load_dir = args.dir
+    method =  args.method
+    save_path = args.save_path
+    approx_pt = f'({args.xp},{args.up})'
+    c_idx = args.c_idx
+    if args.k is None:
+        args.k = -1
+    k_idx = args.k - 1 if args.k > 0 else -1
+    animate(load_dir, save_path, method, approx_pt, c_idx, k_idx)
+
+
+if __name__ == '__main__':
+    main()
--- parsing.py
+++ parsing.py
@@ -82,3 +82,81 @@ def getParsedArgs_plot(default_dir: str, description: str):
     args.dir = fixDirString(args.dir)
     print(f'Loading data from: {args.dir}')
     return args
+
+
+def getParsedArgs_animate(default_dir: str, description: str):
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-D', '--dir', type=str,
+                        default=default_dir,
+                        help='Specify path to directory from which data will be loaded.')
+    parser.add_argument('-m', '--mkdirs', action='store_true',
+                        help='Create SAVE-DIR if it does not exist.')
+    parser.add_argument('-o', '--overwrite', action='store_true',
+                        help='Overwrite contents of save_path if it exists.')
+    group = parser.add_argument_group('Method Selection',
+        """
+        Specify the to use for the animation. Use -method to select from
+        predefined methods or use -xp, -up, -c, and -k to specify custom values.
+        """
+        )
+    opts = ['best_aff', 'best_lin', 'nom_aff', 'nom_lin', 'manual']
+    group.add_argument('-method', type=str, choices=opts, default='nom_lin',
+                       help='Specify method (if manual - must define xp, up, c_idx, and k)')
+    group.add_argument('-xp', type=str, choices=['xt','xe'],
+                       help='Specify anchor state xp')
+    group.add_argument('-up', type=str, choices=['ut','ue'],
+                       help='Specify anchor input up')
+    group.add_argument('-c', '--c-idx', type=int,
+                       help='Specify index of c (0-10 or -1 for "best")')
+    group.add_argument('-k', type=int,
+                       help='Specify value of k (1-100 or -1 for "best")')
+    parser.add_argument('save_path', nargs='?', type=str,
+                        help='Where to save the MP4 animation. If not provided, the animation will be displayed live.')
+    args = parser.parse_args()
+    args.dir = fixDirString(args.dir)
+    print(f'Loading data from: {args.dir}')
+
+    if args.method == 'manual':
+        if args.xp is None:
+            raise ValueError('xp is required when using manual mode')
+        if args.up is None:
+            raise ValueError('up is required when using manual mode')
+        if args.c_idx is None:
+            raise ValueError('c_idx is required when using manual mode')
+        if args.k is None:
+            raise ValueError('k is required when using manual mode')
+
+    if args.c_idx is not None:
+        if not isinstance(args.c_idx, int):
+            raise TypeError(f'Invalid c_idx: {args.c_idx} (should be an integer)')
+        if not -1 <= args.c_idx <= 10:
+            raise ValueError(f'Invalid c_idx: {args.c_idx} (should be between 0 and 10 or -1 for "best")')
+    if args.k is not None:
+        if not isinstance(args.k, int):
+            raise TypeError(f'Invalid k_idx: {args.k} (should be an integer)')
+        if not -1 <= args.k <= 100 and args.k != 0:
+            raise ValueError(f'Invalid k_idx: {args.k} (should be between 1 and 100 or -1 for "best")')
+
+    if args.save_path is not None:
+        if not args.save_path.endswith('.mp4'):
+            raise ValueError(f'Invalid save path: {args.save_path} (should end with .mp4)')
+        if not len(args.save_path) > 4:
+            raise ValueError(f'Invalid save path: {args.save_path} (must specify a filename)')
+        save_dir = os.path.dirname(args.save_path)
+        if not os.path.exists(save_dir) and not args.mkdirs:
+            print(f'Directory does not exist: {save_dir}')
+            ans = input('Create it? [Y/n]: ')
+            if len(ans) == 0: ans = 'y'
+            if not ans.startswith(('y','Y')):
+                msg = f'Create {save_dir} first or use -m flag to create it.'
+                raise NotADirectoryError(msg)
+            os.makedirs(save_dir)
+
+        if os.path.exists(args.save_path) and not args.overwrite:
+            ans = input(f'{args.save_path} contains data. Do you wish to overwrite it? [y/N]: ')
+            if len(ans) == 0: ans = 'n'
+            if not ans.startswith(('y','Y')):
+                msg = f'Move contents of {args.save_path}, choose a different SAVE-PATH, or use -o flag'
+                raise FileExistsError(msg)
+
+    return args
